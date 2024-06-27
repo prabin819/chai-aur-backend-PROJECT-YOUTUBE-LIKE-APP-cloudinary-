fab3afda995add5f57fb09cf5130cd6eba2273e4
Prabin <manandharprabin819@gmail.com>
2024-06-27 16:15:19 +0545

    usermodel and video model created, with use of schema methods for bcrypt and jwt and inntro to mongoose-aggregate-paginate-v2, vid-10

---
+++ src/models/video.model.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true, //cloudinay url
+    },
+    thumbnail: {
+      type: String, //cloudinay url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      //cloudinay url
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
