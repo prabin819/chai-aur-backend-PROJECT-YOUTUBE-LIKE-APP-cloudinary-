5bfd744b3090fe1db178aff55b49ee4b630d09e7
Prabin <manandharprabin819@gmail.com>
2024-06-29 12:55:26 +0545

    postman usage and a bug debugged for optional coverImage (coverImageLocalPath) vid-15

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,8 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     //file has been uploaded successfully
-    console.log("file has been uploaded on cloudinary", response.url);
+    //console.log("file has been uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation got failed
